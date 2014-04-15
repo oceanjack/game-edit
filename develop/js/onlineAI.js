e37ba699c23b0b5592bb4947a036939b2219c43b
oceanjack <oceanjack.app@gmail.com>
2014-04-15 10:57:30 +0800

    添加变换地图变化到的逻辑

--- develop/js/onlineAI.js
+++ develop/js/onlineAI.js
@@ -2,6 +2,7 @@ goog.provide('ocean.onlineAI.OnlineAI');
 
 goog.require('goog.dom');
 goog.require('goog.events');
+goog.require('goog.style');
 
 
 goog.scope(function() {
@@ -34,6 +35,8 @@ goog.scope(function() {
     this.elements_.defsize_ = goog.dom.getElement('defsize');
     this.elements_.defsizeX_ = goog.dom.getElementsByClass('defsize')[0];
     this.elements_.defsizeY_ = goog.dom.getElementsByClass('defsize')[1];
+    this.elements_.sizeArea_ = goog.dom.getElementByClass('size');
+    this.elements_.menuList_ = goog.dom.getElementsByClass('menulist');
   };
 
 
@@ -49,6 +52,9 @@ goog.scope(function() {
       el.defsizeX_.value,
       el.defsizeY_.value
     ];
+    goog.style.setStyle(el.menuList_[0], 'display', 'none');
+    goog.style.setStyle(el.sizeArea_, 'display', 'none');
+    goog.style.setStyle(el.menuList_[1], 'display', 'block');
   };
 
 
