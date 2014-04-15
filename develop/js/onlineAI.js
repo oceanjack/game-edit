7481ae11fb28b4e3ba407500b71764a3df9fec65
oceanjack <oceanjack.app@gmail.com>
2014-04-15 10:25:29 +0800

    add defsize buttonm

--- develop/js/onlineAI.js
+++ develop/js/onlineAI.js
@@ -1,5 +1,8 @@
 goog.provide('ocean.onlineAI.OnlineAI');
 
+goog.require('goog.dom');
+goog.require('goog.events');
+
 
 goog.scope(function() {
   var exports = ocean.onlineAI;
@@ -10,7 +13,42 @@ goog.scope(function() {
   };
 
 
+  exports.OnlineAI.prototype.elements_ = null;
+  exports.OnlineAI.prototype.size_ = null;
+
+
   exports.OnlineAI.prototype.init = function() {
+    this.initVar();
+    this.getElements();
+    this.addEvents();
+  };
+
+
+  exports.OnlineAI.prototype.initVar = function() {
+    this.elements_ = {};
+    this.size_ = [20, 20];
+  };
+
+
+  exports.OnlineAI.prototype.getElements = function() {
+    this.elements_.defsize_ = goog.dom.getElement('defsize');
+    this.elements_.defsizeX_ = goog.dom.getElementsByClass('defsize')[0];
+    this.elements_.defsizeY_ = goog.dom.getElementsByClass('defsize')[1];
+  };
+
+
+  exports.OnlineAI.prototype.addEvents = function() {
+    var el = this.elements_;
+    goog.events.listen(el.defsize_, 'click', this.defsize, false, this);
+  };
+
+
+  exports.OnlineAI.prototype.defsize = function() {
+    var el = this.elements_;
+    this.size_ = [
+      el.defsizeX_.value,
+      el.defsizeY_.value
+    ];
   };
 
 
