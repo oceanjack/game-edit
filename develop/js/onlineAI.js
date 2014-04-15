a7fc07a9df859c4a4139f6e41ecd6fa06f8cae1a
oceanjack <oceanjack.app@gmail.com>
2014-04-15 17:36:10 +0800

    add map by drop

--- develop/js/onlineAI.js
+++ develop/js/onlineAI.js
@@ -34,9 +34,11 @@ goog.scope(function() {
 
   exports.OnlineAI.prototype.getElements = function() {
     this.elements_.defsize_ = goog.dom.getElement('defsize');
+    this.elements_.canvas_ = goog.dom.getElement('canvas');
     this.elements_.defsizeX_ = goog.dom.getElementsByClass('defsize')[0];
     this.elements_.defsizeY_ = goog.dom.getElementsByClass('defsize')[1];
     this.elements_.sizeArea_ = goog.dom.getElementByClass('size');
+    this.elements_.mapImgDiv_ = goog.dom.getElementByClass('mapimgdiv');
     this.elements_.mapImg_ = goog.dom.getElementByClass('mapimg');
     this.elements_.menuList_ = goog.dom.getElementsByClass('menulist');
   };
@@ -45,6 +47,13 @@ goog.scope(function() {
   exports.OnlineAI.prototype.addEvents = function() {
     var el = this.elements_;
     goog.events.listen(el.defsize_, 'click', this.defsize, false, this);
+    goog.events.listen(el.mapImgDiv_, 'dragover', this.onDragOver, false ,this);
+    goog.events.listen(el.mapImgDiv_, 'drop', this.onFileSelect, false ,this);
+  };
+
+
+  exports.OnlineAI.prototype.settings = function() {
+    var el = this.elements_;
   };
 
 
@@ -56,12 +65,55 @@ goog.scope(function() {
     ];
     goog.style.setStyle(el.menuList_[0], 'display', 'none');
     goog.style.setStyle(el.sizeArea_, 'display', 'none');
+    goog.style.setStyle(el.mapImgDiv_, 'display', 'none');
+    goog.style.setStyle(el.canvas_, 'display', 'block');
     goog.style.setStyle(el.menuList_[1], 'display', 'block');
   };
 
 
-  exports.OnlineAI.prototype.settings = function() {
+  exports.OnlineAI.prototype.onDragOver = function(e) {
+    e = e.event_;
+    e.stopPropagation();
+    e.preventDefault();
+    e['dataTransfer']['dropEffect'] = 'copy';
+  };
+
+
+  exports.OnlineAI.prototype.onFileSelect = function(e) {
+    e = e.event_;
+    var this_ = this;
     var el = this.elements_;
+    var onImageRendered = function(e) {
+      var img = e.target || e.srcElement;
+      var width = window.getComputedStyle(el.mapImgDiv_)['width'];
+      var height = window.getComputedStyle(el.mapImgDiv_)['height'];
+      width = width.substr(width, width.length - 2);
+      height = height.substr(height, height.length - 2);
+      el.mapImg_.width = width;
+      el.mapImg_.src = img.src;
+      el.canvas_.width = width;
+      el.canvas_.height = height;
+
+      var context = el.canvas_.getContext('2d');
+      context.clearRect(0, 0, width, height);
+      context.drawImage(el.mapImg_, 0, 0, el.mapImg_.width, el.mapImg_.height);
+    };
+    var onFileReaded = function(e) {
+      var img = new Image();
+      img.onload = onImageRendered;
+      img.src = e.target.result;
+    };
+    e.stopPropagation();
+    e.preventDefault();
+    var container = e.dataTransfer || e.target;	//选择文件和拖放的差别
+    var file = container.files[0];
+    if (!/image/.test(file.type)) {
+      alert('That\'s not an image file!');
+      return;
+    }
+    var reader = new FileReader();
+    reader.onload = onFileReaded;
+    reader.readAsDataURL(file);
   };
 
 
