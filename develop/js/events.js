7fd79f79baef553f6008ae417e8299bee544c3dd
oceanjack <oceanjack.app@gmail.com>
2014-05-04 16:46:11 +0800

    add events.js

---
+++ develop/js/events.js
@@ -0,0 +1,14 @@
+goog.provide('ocean.onlineAI.Events');
+
+goog.require('goog.events');
+
+goog.require('ocean.onlineAI.Data');
+goog.require('ocean.onlineAI.One');
+
+goog.scope(function() {
+  var exports = ocean.onlineAI;
+  var dataModel = exports.Data;
+  
+  exports.Events = function() {
+  };
+});
